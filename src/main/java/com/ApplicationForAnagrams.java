e7eb38bcee2b23d244cee3c7e69f0fc55cd606bf
Diana Devasia <diana14devasia@gmail.com>
2020-05-27 20:37:16 -0400

    Setup spring boot dependencies

---
+++ src/main/java/com/ApplicationForAnagrams.java
@@ -0,0 +1,11 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationForAnagrams {
+    public static void main(String args[]){
+        SpringApplication.run(ApplicationForAnagrams.class, args);
+    }
+}
